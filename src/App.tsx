b3aff6b6ad04579ae19683b372eb6ec179011058
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-04-23 22:14:24 -0400

    repsonsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { Button } from "@chakra-ui/react"
-
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return(<div>
-    <Button colorScheme="blue">
-      test
-    </Button>
-  </div>)
-}
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bgColor="dodgerblue">
+        nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bgColor="navy">
+          aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bgColor="coral">
+        main
+      </GridItem>
+    </Grid>
+  );
+};
 
-export default App
+export default App;
